3f3d6a746cb4d9ba2e039edb512e6a8bd3313e71
Tom Chen <developer@tomchentw.com>
2015-08-31 15:20:10 +0800

    docs(gh-pages): add images for future work

--- examples/gh-pages/views/index.html.js
+++ examples/gh-pages/views/index.html.js
@@ -8,11 +8,37 @@ import {
   ReactRenderToStringEntry,
 } from "../../../lib/entry";
 
+function unsupportedIconsFeature () {
+  if (!!WebpackScriptEntry.abcdefgblabla) {
+    // These features aren't supported atm.
+    return [
+      /* Apple iOS devices and Google Android Devices */
+      <link rel="apple-touch-icon-precomposed" href="/path/to/a-hashtag-152-211218.png" />,
+      /* IE 10 Metro Tile Icon */
+      <meta name="msapplication-TileColor" content="#ffffff" />,
+      <meta name="msapplication-TileImage" content="/path/to/a-hashtag-144-211218.png" />,
+      /* Everything Else */
+      <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/path/to/a-hashtag-152-211218.png" />,
+      <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/path/to/a-hashtag-144-211218.png" />,
+      <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/path/to/a-hashtag-120-211218.png" />,
+      <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/path/to/a-hashtag-114-211218.png" />,
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/path/to/a-hashtag-72-211218.png" />,
+      <link rel="apple-touch-icon-precomposed" href="/path/to/a-hashtag-57-211218.png" />,
+      <link rel="icon" href="/path/to/a-hashtag-32-211218.png" sizes="32x32" />,
+      /* end of icons */
+    ];
+  } else {
+    return null;
+  }
+}
+
 export default (
   <html>
     <head>
       <title>React Google Maps | tomchentw</title>
+      {/* http://www.freefavicon.com/freefavicons/objects/iconinfo/a-hashtag-152-211218.html */}
       <link href="favicon.ico" rel="shortcut icon" />
+      {unsupportedIconsFeature()}
       <WebpackStyleEntry
         chunkName="client"
         chunkFilepath="./scripts/client.js"
